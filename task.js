e4c10dc1408a4ed670be59ebebccdca627a9621c
Igor <hdd500t@yandex.ru>
2022-05-24 21:57:41 +0300

    work on bugs

--- task.js
+++ task.js
@@ -9,7 +9,7 @@ for (const product of products) {
     const productQuantityControlsInc = productQuantityControls.querySelector('.product__quantity-control_inc');
 
     productQuantityControlsDec.addEventListener('click', () => {
-        if (Number(productQuantityControlsValue.textContent) > 0) {
+        if (Number(productQuantityControlsValue.textContent) > 1) {
             productQuantityControlsValue.textContent = Number(productQuantityControlsValue.textContent) - 1;
         }
     });
@@ -20,39 +20,33 @@ for (const product of products) {
 
     const productAdd = productQuantity.querySelector('.product__add');
     productAdd.addEventListener('click', () => {
-        const cart = document.querySelector('.cart');
-        const cartProducts = cart.querySelectorAll('.cart__products .cart__product');
-        
-        idList = [];
-        for (let cartProduct of cartProducts) {
-            idList.push(Number(cartProduct.dataset.id));
+
+        const cartProducts = document.getElementsByClassName('cart__product');
+        const cartProductsArray = Array.from(cartProducts)
+        const cartProductArray = cartProductsArray.find((findProduct) => {
+            return findProduct.dataset.id === product.dataset.id;
+        });        
+
+        if (cartProductArray != undefined) {
+            cartProductArray.querySelector('.cart__product-count').textContent = Number(cartProductArray.textContent) + Number(productQuantityControlsValue.textContent);
         }
+        
+        else {
+            // создаем html элемент
+            const newProduct = document.createElement('div');
+            newProduct.className = 'cart__product';
+            newProduct.dataset.id = product.dataset.id;
+            document.querySelector('.cart .cart__products').appendChild(newProduct);
 
-        if (idList.includes(Number(product.dataset.id))) {
-            for (let cartProduct of cartProducts) {
-                if (cartProduct.dataset.id == product.dataset.id) {
-                    cartProduct.querySelector('.cart__product-count').textContent = Number(cartProduct.querySelector('.cart__product-count').textContent) + Number(productQuantityControlsValue.textContent);
-                    break;
-                }
-            }
-        } else {
-            if (Number(productQuantityControlsValue.textContent) > 0) {
-                // создаем html элемент
-                const newProduct = document.createElement('div');
-                newProduct.className = 'cart__product';
-                newProduct.dataset.id = product.dataset.id;
-                document.querySelector('.cart .cart__products').appendChild(newProduct);
+            const newProductImage = document.createElement('img');
+            newProductImage.className = 'cart__product-image';
+            newProductImage.src = product.querySelector('img').src;
+            newProduct.appendChild(newProductImage);
 
-                const newProductImage = document.createElement('img');
-                newProductImage.className = 'cart__product-image';
-                newProductImage.src = product.querySelector('img').src;
-                newProduct.appendChild(newProductImage);
-
-                const newProductCount = document.createElement('div');
-                newProductCount.className = 'cart__product-count';
-                newProductCount.textContent = productQuantityControlsValue.textContent;
-                newProduct.appendChild(newProductCount);
-            }
+            const newProductCount = document.createElement('div');
+            newProductCount.className = 'cart__product-count';
+            newProductCount.textContent = productQuantityControlsValue.textContent;
+            newProduct.appendChild(newProductCount);
         }
     });
 }
